fdb1f1905fadca3ad1eb53d540181831ed5baa0f
Cairo <caio12013marcelo@gmail.com>
2026-06-18 19:47:11 -0300

    1234

---
+++ ExerciciosVetores/src/ex06.java
@@ -0,0 +1,9 @@
+public class ex06 {
+    public static void main(String[] args) {
+        int[] v = {10, 20, 30, 40, 50};
+
+        for (int i=4; i>=0;i--){
+            System.out.println(v[i]);
+        }
+    }
+}
