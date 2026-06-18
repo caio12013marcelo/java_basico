c33c8f81b396aed93fe8920e12b835f1caf44b44
Cairo <caio12013marcelo@gmail.com>
2026-06-18 19:47:11 -0300

    1234

--- ExerciciosVetores/src/Ex05.java
+++ ExerciciosVetores/src/Ex05.java
@@ -1,5 +1,11 @@
 public class Ex05 {
     public static void main(String[] args) {
+        int[] v = { 3, 7, 1, 9, 4, 6, 2, 8, 5 };
 
+        for (int i = 1; i < v.length; i += 2) {
+            if(i % 2 !=0){
+                System.out.println(v[i]+ " = "+i);
+            }
+        }
     }
 }
